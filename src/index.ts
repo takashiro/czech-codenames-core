bacc1545895aa38ad255796c00368898704fa7bf
Kazuichi Takashiro <takashiro@qq.com>
2021-07-26 21:56:27 +0800

    Add interface Config

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { default as Color } from './Color';
+export { default as Config } from './Config';
 export { default as Context } from './Context';
 export { default as NameCard } from './NameCard';
